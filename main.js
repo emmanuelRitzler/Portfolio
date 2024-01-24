520469c37c98d062975ed44f97dea825ee9be3f0
Emmanuel Ritzler <ritzler.emmanuel@gmail.com>
2024-01-25 00:36:23 +0100

    Add dark mode to home page

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-// const changeColor = document.querySelector('.main-container');
-// const switchMode = document.querySelector('.darkmode-slider');
-
-// switchMode.addEventListener('click', () => {
-//     changeColor.classList.toggle('darkmode');
-//     changeColor.classList.toggle('lightmode');
-// });
+// Saves the preferences of the user on his computer
+let darkMode = localStorage.getItem('darkMode');
 
 const switchMode = document.querySelector('.darkmode-slider');
 
-switchMode.addEventListener('click', moveButton => {
-    switchMode.style.justifyContent = switchMode.style.justifyContent === 'flex-start' ? 'flex-end' : 'flex-start';
-});
 
+
+const enableDarkMode = () => {
+    localStorage.setItem('darkMode', 'enabled');
+};
+const disableDarkMode = () => {
+    localStorage.setItem('darkMode', 'disabled');
+};
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+};
+
+const switchTheme = () => {
+    const currentJustifyContent = switchMode.style.justifyContent || 'flex-start';
+    switchMode.style.justifyContent = currentJustifyContent === 'flex-start' ? 'flex-end' : 'flex-start';
+
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute('data-theme');
+    const newTheme = (dataTheme === "light") ? "dark" : "light";
+
+    darkMode = localStorage.getItem('darkMode');
+    if (dataTheme === "light") {
+        enableDarkMode;
+    }
+    else {
+        disableDarkMode;
+    }
+    rootElem.setAttribute('data-theme', newTheme)
+    console.log(dataTheme);
+}
+
+switchMode.addEventListener('click', switchTheme);
 
